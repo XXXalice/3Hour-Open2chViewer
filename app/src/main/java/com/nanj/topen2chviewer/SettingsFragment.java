ca77fccac3f48d1d4aaf3be4737f6f906a4d3082
NanJ-Dev <nanjdev334@gmail.com>
2020-07-21 05:34:37 +0900

    Remove OnPrefenceChengeListener

--- app/src/main/java/com/nanj/topen2chviewer/SettingsFragment.java
+++ app/src/main/java/com/nanj/topen2chviewer/SettingsFragment.java
@@ -3,27 +3,13 @@ package com.nanj.topen2chviewer;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.Preference.OnPreferenceClickListener;
 import androidx.preference.PreferenceFragmentCompat;
-import java.lang.Object;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
   @Override
   public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
     setPreferencesFromResource(R.xml.preferences, rootKey);
-
-    // テーマ選択スイッチのListener
-    Preference themePreference = findPreference("darkorlight");
-    themePreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-      @Override
-      public boolean onPreferenceChange(Preference preference, Object newValue) {
-        // アクティビティを再起動する
-        getActivity().finish();
-        startActivity(new Intent(getActivity().getApplicationContext(), SettingsActivity.class));
-        return true;
-      }
-    });
 
     // 「About」のListener
     Preference aboutPreference = findPreference("about");
