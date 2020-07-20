d3efcc84abb8ec42147b873ee52e7d06332b1ad7
NanJ-Dev <nanjdev334@gmail.com>
2020-07-20 23:52:48 +0900

    Fix Error

--- app/src/main/java/com/nanj/topen2chviewer/SettingsFragment.java
+++ app/src/main/java/com/nanj/topen2chviewer/SettingsFragment.java
@@ -1,11 +1,24 @@
 package com.nanj.topen2chviewer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceClickListener;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
   @Override
   public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
     setPreferencesFromResource(R.xml.preferences, rootKey);
+    
+    // 「About」のListener
+    Preference aboutPreference = findPreference("about");
+    aboutPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+      public boolean onPreferenceClick(Preference preference) {
+        // AboutActivityに飛ぶ
+        startActivity(new Intent(SettingsFragment.this, AboutActivity.class));
+        return true;
+      }
+    });
   }
 }
