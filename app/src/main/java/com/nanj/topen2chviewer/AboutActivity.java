62bdd8324072fd072aff3c103e849aaa6360827f
NanJ-Dev <nanjdev334@gmail.com>
2020-07-21 14:50:00 +0900

    Fix Error

--- app/src/main/java/com/nanj/topen2chviewer/AboutActivity.java
+++ app/src/main/java/com/nanj/topen2chviewer/AboutActivity.java
@@ -3,7 +3,6 @@ package com.nanj.topen2chviewer;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import mehdi.sakout.aboutpage.AboutPage;
 
 public class AboutActivity extends AppCompatActivity {
@@ -13,7 +12,8 @@ public class AboutActivity extends AppCompatActivity {
 
     // Aboutページを作成
     boolean darkMode;
-    if (AppCompatDelegate.getDefaultNightMode() = AppCompatDelegate.MODE_NIGHT_YES) {
+    int darkModeFlags = getContext().getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+    if (darkModeFlags == Configuration.UI_MODE_NIGHT_YES) {
       darkMode = true;
     } else {
       darkMode = false;
