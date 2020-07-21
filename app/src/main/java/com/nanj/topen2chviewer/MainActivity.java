682e26dc63820478a6d786c7d6eae8547069a8f6
NanJ-Dev <nanjdev334@gmail.com>
2020-07-21 18:27:38 +0900

    Test Commit

--- app/src/main/java/com/nanj/topen2chviewer/MainActivity.java
+++ app/src/main/java/com/nanj/topen2chviewer/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
     } else {
       AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
-    getDelegate().applyDayNight();
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
