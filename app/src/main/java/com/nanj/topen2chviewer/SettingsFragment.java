e60155b0440e30ac4301baee6e88903f92dea111
NanJ-Dev <nanjdev334@gmail.com>
2020-07-21 16:25:27 +0900

    Add Material Dialog

--- app/src/main/java/com/nanj/topen2chviewer/SettingsFragment.java
+++ app/src/main/java/com/nanj/topen2chviewer/SettingsFragment.java
@@ -9,6 +9,7 @@ import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.Preference.OnPreferenceClickListener;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
   @Override
@@ -21,7 +22,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
       @Override
       public boolean onPreferenceChange(Preference preference, Object newValue) {
         // トーストを出す
-        Toast.makeText(getActivity().getApplicationContext(), "次回起動時から有効となります", Toast.LENGTH_LONG).show();
+        Toast.makeText(getActivity().getApplicationContext(), "次回起動時から有効となります。", Toast.LENGTH_LONG).show();
         return true;
       }
     });
@@ -37,16 +38,27 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     });
 
     // 「デフォルトの設定に戻す」のListener
-    Preference backDefaultPreference = findPreference("backdefault");
-    backDefaultPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+    Preference restoreDefaultPreference = findPreference("restoredefault");
+    restoreDefaultPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
       public boolean onPreferenceClick(Preference preference) {
         // デフォルトの設定に戻す
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("homepage", "https://open2ch.net/sp/");
-        editor.putBoolean("darkorlight", true);
-        editor.apply();
-        Toast.makeText(getActivity().getApplicationContext(), "デフォルトの設定に戻されました", Toast.LENGTH_LONG).show();
+        new MaterialAlertDialogBuilder(getActivity().getApplicationContext())
+            .setTitle("確認")
+            .setMessage("デフォルトの設定に戻します。\nよろしいですか？")
+            .setNegativeButton("キャンセル", null)
+            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+              public void onClick(DialogInterface dialogInterface, int i) {
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("homepage", "https://open2ch.net/sp/");
+                editor.putBoolean("darkorlight", true);
+                editor.apply();
+                Toast.makeText(getActivity().getApplicationContext(), "デフォルトの設定に戻されました。", Toast.LENGTH_LONG).show();
+                finish();
+                startActivity(getActivity().getIntent());
+              }
+            })
+            .show();
         return true;
       }
     });
