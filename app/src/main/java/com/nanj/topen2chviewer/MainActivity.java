a21059fb91b215aa7546cb9edd64a6dfa9a132b7
NanJ-Dev <nanjdev334@gmail.com>
2020-07-21 15:41:06 +0900

    Fix Slow Screen

--- app/src/main/java/com/nanj/topen2chviewer/MainActivity.java
+++ app/src/main/java/com/nanj/topen2chviewer/MainActivity.java
@@ -24,7 +24,6 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
 
     // テーマを変更する
     SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
@@ -34,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
       AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
 
+    super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     // アップデートを確認
