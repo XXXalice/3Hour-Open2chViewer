89467ee14de19968c70d3209123606258b5e6c5c
NanJ-Dev <nanjdev334@gmail.com>
2020-07-21 17:25:56 +0900

    Add setText

--- app/src/main/java/com/nanj/topen2chviewer/SettingsFragment.java
+++ app/src/main/java/com/nanj/topen2chviewer/SettingsFragment.java
@@ -1,16 +1,13 @@
 package com.nanj.topen2chviewer;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
+import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.Preference.OnPreferenceClickListener;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.PreferenceManager;
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
   @Override
@@ -43,23 +40,9 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     restoreDefaultPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
       public boolean onPreferenceClick(Preference preference) {
         // デフォルトの設定に戻す
-        new MaterialAlertDialogBuilder(getActivity().getApplicationContext())
-            .setTitle("確認")
-            .setMessage("デフォルトの設定に戻します。\nよろしいですか？")
-            .setNegativeButton("キャンセル", null)
-            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-              public void onClick(DialogInterface dialogInterface, int i) {
-                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("homepage", "https://open2ch.net/sp/");
-                editor.putBoolean("darkorlight", true);
-                editor.apply();
-                Toast.makeText(getActivity().getApplicationContext(), "デフォルトの設定に戻されました。", Toast.LENGTH_LONG).show();
-                getActivity().finish();
-                startActivity(getActivity().getIntent());
-              }
-            })
-            .show();
+        EditTextPreference homePage = findPreference("homepage");
+        homePage.setText("https://open2ch.net/sp/");
+        Toast.makeText(getActivity().getApplicationContext(), "デフォルトの設定に戻されました。", Toast.LENGTH_LONG).show();
         return true;
       }
     });
